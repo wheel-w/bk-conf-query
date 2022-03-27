2d95e201e7af1f8d741b9f3940959b0505a3e22e
wheel-w <2425024187@qq.com>
2022-03-27 14:41:08 +0800

    minor: add app config_query

--- config/default.py
+++ config/default.py
@@ -32,9 +32,12 @@ from blueapps.conf.log import get_logging_config_dict
 # )
 
 # 请在这里加入你的自定义 APP
-INSTALLED_APPS += (  # noqa
+INSTALLED_APPS += (
     "home_application",
     "mako_application",
+    "config_query",
+    "django_filters",
+    "rest_framework",
 )
 
 # 这里是默认的中间件，大部分情况下，不需要改动
